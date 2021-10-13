f1bd0bcef26d061be14ccd5c643f5b206b07c5a3
Silvano Santana <jnkppl@hotmail.com>
2021-10-13 19:37:54 -0300

    feat: ensure cors is enabled

--- src/application/config/app.ts
+++ src/application/config/app.ts
@@ -1,10 +1,14 @@
 import express, { Express } from 'express';
 import { bodyParser } from '../middlewares/bodyParser';
+import { contentType } from '../middlewares/contentType';
+import { cors } from '../middlewares/cors';
 
 export const setupApp = async (): Promise<Express> => {
   const app = express();
 
   app.use(bodyParser);
+  app.use(cors);
+  app.use(contentType);
 
   return app;
 };
