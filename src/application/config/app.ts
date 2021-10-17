9602a7f7f6c265ab43cc6d2f17c38da61504c09b
Silvano Santana <jnkppl@hotmail.com>
2021-10-17 16:28:00 -0300

    refactor: middleware refactor

--- src/application/config/app.ts
+++ src/application/config/app.ts
@@ -1,18 +1,13 @@
 import express, { Express } from 'express';
+import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
 import setupSwagger from './swagger';
-import { bodyParser } from '../middlewares/bodyParser';
-import { contentType } from '../middlewares/contentType';
-import { cors } from '../middlewares/cors';
 
 export const setupApp = async (): Promise<Express> => {
   const app = express();
 
   setupSwagger(app);
-  app.use(bodyParser);
-  app.use(cors);
-  app.use(contentType);
-
+  setupMiddlewares(app);
   setupRoutes(app);
 
   return app;
